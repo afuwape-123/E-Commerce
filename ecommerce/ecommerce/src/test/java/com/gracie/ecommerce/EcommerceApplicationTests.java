003806b2a091927d53630a83166d7a0e35ec0e73
afuwape <giloadewale123@gmail.com>
2020-12-01 00:33:45 +0000

    first commit

---
+++ ecommerce/ecommerce/src/test/java/com/gracie/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracie.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
