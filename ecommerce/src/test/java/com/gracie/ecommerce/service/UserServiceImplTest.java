38a1d1aa38d270380f6380d991a37fd2d0f5317b
Grace <gracebolutife@gmail.com>
2020-12-08 18:33:24 +0100

    userService tested

---
+++ ecommerce/src/test/java/com/gracie/ecommerce/service/UserServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.gracie.ecommerce.service;
+
+import com.gracie.ecommerce.data.model.User;
+import com.gracie.ecommerce.data.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.when;
+
+
+class UserServiceImplTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    User user;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        user = new User();
+    }
+
+    @Test
+    void mockTestSaveUser(){
+        when(userRepository.save(user)).thenReturn(user);
+
+        userService.saveUser(user);
+
+       verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    void mockDeleteUserTest(){
+        doNothing().when(userRepository).delete(user);
+
+        userService.deleteUser(user);
+
+        verify(userRepository, times(1)).delete(user);
+    }
+}
